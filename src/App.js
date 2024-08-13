6e2449ecc93df5a7581b244fc9721b15544e2e27
swapnesh <swapnesha839@gmail.com>
2024-08-13 14:14:34 +0530

    with issue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import MindARViewer from './mindar-viewer';
+import MindARThreeViewer from './mindar-three-viewer';
 
 function App() {
+  const [started, setStarted] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Example React component with <a href="https://github.com/hiukim/mind-ar-js" target="_blank"  rel="noreferrer">MindAR</a></h1>
+
+      <div className="control-buttons">
+        {started === null && <button onClick={() => {setStarted('aframe')}}>Start AFRAME version</button>}
+        {started === null && <button onClick={() => {setStarted('three')}}>Start ThreeJS version</button>}
+        {started !== null && <button onClick={() => {setStarted(null)}}>Stop</button>}
+      </div>
+
+      {started === 'aframe' && (
+        <div className="container">
+          <MindARViewer/>
+          <video></video>
+        </div>
+      )}
+
+      {started === 'three' && (
+        <div className="container">
+          <MindARThreeViewer />
+        </div>
+      )}
     </div>
   );
 }
